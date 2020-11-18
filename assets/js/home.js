736d7aedf42c5ee86cf3f8045ef1fbd33fa240fc
J-O-B <Jonathanmichaelobrien@gmail.com>
2020-11-18 17:15:26 +0000

    changed front page content layout, added github api

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,7 +1,16 @@
+//Auto Play Video on mouse over
 document.getElementById("logoVid").addEventListener("mouseover", function() {
 	this.play();
 });
 
 document.getElementById("logoVid").addEventListener("mouseleave", function() {
 	this.pause();
+});
+
+//Homepage 3 reasons expand on click
+document.getElementsById("space-top").on("click", function() {
+	$(this).removeClass("hide").slideToggle(1000 );
+});
+document.getElementsById(".space-top").addEventListener("mouseleave", function() {
+	$(this).addClass("hide").slideToggle( 1000 );
 });
