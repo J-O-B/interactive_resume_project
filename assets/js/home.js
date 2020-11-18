32707dea66451b52b1df89a13d6053519d9e8975
J-O-B <Jonathanmichaelobrien@gmail.com>
2020-11-18 03:08:10 +0000

    changed logo area on all pages. Page content needs update next

---
+++ assets/js/home.js
@@ -0,0 +1,7 @@
+document.getElementById("logoVid").addEventListener("mouseover", function() {
+	this.play();
+});
+
+document.getElementById("logoVid").addEventListener("mouseleave", function() {
+	this.pause();
+});
