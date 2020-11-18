74467c0b7f6040c1dd26710f4f31189865723d30
J-O-B <Jonathanmichaelobrien@gmail.com>
2020-11-18 18:00:19 +0000

    added repos function to github page

--- assets/js/home.js
+++ assets/js/home.js
@@ -6,11 +6,3 @@ document.getElementById("logoVid").addEventListener("mouseover", function() {
 document.getElementById("logoVid").addEventListener("mouseleave", function() {
 	this.pause();
 });
-
-//Homepage 3 reasons expand on click
-document.getElementsById("space-top").on("click", function() {
-	$(this).removeClass("hide").slideToggle(1000 );
-});
-document.getElementsById(".space-top").addEventListener("mouseleave", function() {
-	$(this).addClass("hide").slideToggle( 1000 );
-});
